5f43cb9692ba5e95396c1711794c31272a50ad56
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-05-01 16:21:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	const table = document.getElementById('sampleTable');
+	const totalRows = table.innerHTML;
+
+	table.innerHTML = `
+		<tr>
+			<td>New Cell1</td> 
+			<td>New Cell2</td>
+		</tr>`;
+	table.innerHTML += totalRows;
 }
